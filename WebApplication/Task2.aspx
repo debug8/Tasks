f53e4bb1c32a4ef895c5682719b107233fc6fa7a
debug8 <33095109+debug8@users.noreply.github.com>
2017-11-05 18:58:32 +0300

    Initial commit

---
+++ WebApplication/Task2.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Task2.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 297px">
+    
+        <asp:DropDownList ID="SelectModeList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="SelectModeList_SelectedIndexChanged" Width="111px" Height="23px">
+            <asp:ListItem></asp:ListItem>
+            <asp:ListItem Value="1">predefined</asp:ListItem>
+            <asp:ListItem Value="2">custom</asp:ListItem>
+        </asp:DropDownList>
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="PredefinedPositionList" Visible ="false" runat="server" Width="114px" Height="23px">
+            <asp:ListItem>position 1</asp:ListItem>
+            <asp:ListItem>position 2</asp:ListItem>
+            <asp:ListItem>position 3</asp:ListItem>
+            <asp:ListItem>position 4</asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="CustomPositionText" Visible ="false" runat="server" Width="110px" Height="17px"></asp:TextBox>    
+    &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="SubmitButton" runat="server" Visible ="false" Text="Save" OnClick="SubmitButton_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication/Task2.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1 {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// form1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// SelectModeList элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList SelectModeList;
+        
+        /// <summary>
+        /// PredefinedPositionList элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList PredefinedPositionList;
+        
+        /// <summary>
+        /// CustomPositionText элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox CustomPositionText;
+        
+        /// <summary>
+        /// SubmitButton элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SubmitButton;
+    }
+}
